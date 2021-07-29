a51f810e325239fe8f3b6874c50db7b27c0aaa12
Iñigo Ochoa <contact@inigochoa.me>
2021-07-29 18:47:23 +0200

    added file transfer function

--- dotfiles/shell/functions.sh
+++ dotfiles/shell/functions.sh
@@ -120,6 +120,38 @@ path_prepend() {
   PATH="$1${PATH:+":$PATH"}"
 }
 
+# Upload file to transfer.sh - https://transfer.sh/
+transfer() {
+  if [ $# -eq 0 ]; then
+    echo -e "No arguments specified.\nUsage:\n  transfer <file|directory>\n  ... | transfer <file_name>">&2
+
+    return 1
+  fi
+
+  if tty -s; then
+    file="$1"
+    file_name=$(basename "$file")
+
+    if [ ! -e "$file" ]; then
+      echo "$file: No such file or directory">&2
+
+      return 1
+    fi
+
+    if [ -d "$file" ]; then
+      file_name="$file_name.zip" ,
+      (cd "$file" && zip -r -q - .)|curl --progress-bar --upload-file "-" "https://transfer.sh/$file_name"|tee /dev/null,
+    else
+      cat "$file"|curl --progress-bar --upload-file "-" "https://transfer.sh/$file_name"|tee /dev/null
+    fi
+  else
+    file_name=$1
+    curl --progress-bar --upload-file "-" "https://transfer.sh/$file_name"|tee /dev/null
+  fi
+
+  echo -e "\n"
+}
+
 # Weather report
 wttr() {
   local request="wttr.in/${1-Vitoria}"
